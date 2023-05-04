b5b4f092d35776e55ecdb110255063b057d7c8e5
rhrubelr <rhrubel246@gmail.com>
2023-05-04 20:22:56 +0600

    added blogs

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -26,9 +26,9 @@ const Blogs = () => {
                     <p className='mt-3 mb-10'>
                         Props and PropTypes are important mechanisms for passing read-only attributes between React components.
 
-                        We can use React props, short for properties, to send data from one component to another. If a component receives the wrong type of props, it can cause bugs and unexpected errors in your app.
+                        We can use React props, short for properties, to send data, from one component to another. If a component receives the wrong type of props, it can cause bugs and unexpected errors in your app.
 
-                        Since JavaScript doesn’t have a built-in type checking solution, many developers use extensions like TypeScript and Flow. However, React has an internal mechanism for props validation called PropTypes. In this article, we’ll learn how to validate props with React PropTypes.
+                        Since JavaScript doese not have a built-in type checking solution, many developers use extensions like TypeScript and Flow. However, React has an internal mechanism for props validation called PropTypes. In this article, we’ll learn how to validate props with React PropTypes.
                     </p>
                 </div>
 
@@ -48,9 +48,9 @@ const Blogs = () => {
                     <hr />
                     <p className='mt-3 mb-10'>
 
-                        A custom hook is a feature in React, a popular JavaScript library for building user interfaces, that allows you to extract reusable logic from a component. It enables you to create a function that encapsulates common stateful logic and can be shared across multiple components.
+                        A custom hook is a features  in React, this is very important a popular JavaScript library for building user interfaces, that allows you to extract reusable logic from a component. It enables you to create a function that encapsulates common stateful logic and can be shared across multiple components.
 
-                        In React, hooks are functions that let you use state and other React features without writing a class. While React provides several built-in hooks like useState and useEffect, you can also create your own custom hooks to abstract and reuse specific logic.
+                        In React, hooks are functions that lets you use state and other React features without writing a class. While React provides several built-in hooks like useState and useEffect, you can also create your own custom hooks to abstract and reuse specific logic.
                     </p>
                 </div>
 
