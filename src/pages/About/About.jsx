b8a0957df95f84aa21a907cfd21d50db1da9ea10
rhrubelr <rhrubel246@gmail.com>
2023-05-04 20:19:54 +0600

    active link added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='  text-center text-5xl py-5'>
-            <h1>This page is coming soon</h1>
+            <h1>This is upcoming about page</h1>
         </div>
     );
 };
