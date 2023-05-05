c7bd644a2b76d7403d25a864f71e6f14d0d6802a
rhrubelr <rhrubel246@gmail.com>
2023-05-05 16:26:34 +0600

    added sencond banner

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className='  text-center text-5xl py-5'>
-            <h1>This is upcoming about page</h1>
+            <h1>This is upcoming about page . Next time this section will be improve worldwide</h1>
         </div>
     );
 };
