0c929a6e0e2bcd1ef295f801057b5560aeb14c98
rhrubelr <rhrubel246@gmail.com>
2023-05-05 16:13:14 +0600

    added view receipe

--- src/pages/Share/Footer/Footer.jsx
+++ src/pages/Share/Footer/Footer.jsx
@@ -20,13 +20,13 @@ const Footer = () => {
     <span className="footer-title">Company</span> 
     <a className="link link-hover">About us</a> 
     <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
+    <a className="link link-hover">hunter</a> 
     <a className="link link-hover">Press kit</a>
   </div> 
   <div>
     <span className="footer-title">Legal</span> 
     <a className="link link-hover">Terms of use</a> 
-    <a className="link link-hover">Privacy policy</a> 
+    <a className="link link-hover">Privacy receipy</a> 
     <a className="link link-hover">Cookie policy</a>
   </div>
 
